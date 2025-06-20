97bd34d8049c0ce175692089eac2a621f44d36de
EngCosta10 <costapmc@gmail.com>
2025-06-20 15:00:51 +0100

    Corrigir caminho para componente Header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/Header';
+import Header from '../src/components/Header';
 
 export default function Home() {
   const handleCheckout = async () => {
